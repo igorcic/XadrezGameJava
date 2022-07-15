45b22f26c23a44b53b6d31536f2ced360b9d72e0
igor bastos <igorbastoscic@gmail.com>
2022-07-14 21:40:24 -0300

    Limpeza de buff e algumas exceptions

--- src/application/UI.java
+++ src/application/UI.java
@@ -10,6 +10,11 @@ import java.util.Scanner;
 public class UI {
 
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
